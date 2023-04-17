bd5559f43fc4d695e0ed2c1bdf75152c196340b6
Jasmine Sarin <66775431+jasminesarin@users.noreply.github.com>
2023-04-17 12:12:43 -0500

    Added map with a marker on ID

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
+// Initialize and add the map
 let map;
 
 async function initMap() {
+  // The location of Institute of Design, IIT
+  const position = { lat: 41.837047066759155, lng: -87.62828512182891 };
+  // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
+  // The map, centered at ID
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Institute of Design, IIT",
   });
 }
 
