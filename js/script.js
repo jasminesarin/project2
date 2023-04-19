f5dad9185c07aabaaddec231f862ac5063ba1741
Jasmine Sarin <66775431+jasminesarin@users.noreply.github.com>
2023-04-19 12:07:00 -0500

    trying polygon

--- js/script.js
+++ js/script.js
@@ -1,31 +1,62 @@
-// Initialize and add the map
 let map;
+let infoWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    position: new google.maps.LatLng(-41.8369733, 87.6282752),
-    zoom: 16,
+    zoom: 5,
+    center: { lat: 41.837, lng: -87.628}, 
+    mapTypeId: "terrain",
   });
 
-  // The marker, positioned at ID
-  const iconImage = "media/icon.png";
-  let marker = google.maps.Marker;
-  marker = new google.maps.Marker({
-    map: map,
-    draggable: true,
-    icon: iconImage,
-    animation: google.maps.Animation.DROP,
-    position: position,
+  // Define the LatLng coordinates for the polygon.
+  const rectangleCoords = [
+    { lat: 41.839, lng: -87.629 }, 
+    { lat: 41.839, lng: -87.623 }, 
+    { lat: 41.831, lng: -87.623 }, 
+    { lat: 41.830, lng: -87.629 },
+  ];
+  // Construct the polygon.
+  const collegeSquare = new google.maps.Polygon({
+    paths: rectangleCoords,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 3,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
   });
-  marker.addListener("click", toggleBounce);
 
-  function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-      marker.setAnimation(null);
-    } else {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
+  collegeSquare.setMap(map);
+  // Add a listener for the click event.
+  collegeSquare.addListener("click", showArrays);
+  infoWindow = new google.maps.InfoWindow();
 }
 
-initMap();
+function showArrays(event) {
+  // Since this polygon has only one path, we can call getPath() to return the
+  // MVCArray of LatLngs.
+  // @ts-ignore
+  const polygon = this;
+  const vertices = polygon.getPath();
+  let contentString =
+    "<b>College Square polygon</b>" +
+    "Clicked location: " +
+    event.latLng.lat() +
+    "," +
+    event.latLng.lng() +
+    " ";
+
+  // Iterate over the vertices.
+  for (let i = 0; i < vertices.getLength(); i++) {
+    const xy = vertices.getAt(i);
+
+    contentString +=
+      " " + "Coordinate " + i + ":  " + xy.lat() + "," + xy.lng();
+  }
+
+  // Replace the info window's content and position.
+  infoWindow.setContent(contentString);
+  infoWindow.setPosition(event.latLng);
+  infoWindow.open(map);
+}
+
+window.initMap = initMap;
